91f77ff13045287f95dee752781b248ac00b5002
phvietan <phvietan@gmail.com>
2023-08-21 22:49:53 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	path, err := exec.LookPath("curl")
+	if err != nil {
+		panic("Command 'curl' not found")
+	}
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		fmt.Println("Received SIGINT: stopping process")
+		os.Exit(0)
+	}()
+
+	args := os.Args
+	args[0] = "curl"
+	for _, arg := range args {
+		if arg == "-h" {
+			fmt.Printf("Usage: %s [options...] <url>\nWill spam 'curl' command until receive SIGINT\nBelow is curl help\n==============================================\n", args[0])
+			syscall.Exec(path, args, os.Environ())
+			break
+		}
+	}
+
+	for {
+		pid, _, _ := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
+		if pid == 0 {
+			syscall.Exec(path, args, os.Environ())
+		} else {
+			proc, err := os.FindProcess(int(pid))
+			if err != nil {
+				panic("Something is wrong in spawning processes")
+			}
+			_, err = proc.Wait()
+			if err != nil {
+				panic(err.Error())
+			}
+		}
+	}
+
+}
