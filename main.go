71c00e6ce251a48a0a0a04106dd4adb623706c13
An Pham <an.pham@linecorp.com>
2023-08-22 15:06:03 +0700

    fix for MacOS

--- main.go
+++ main.go
@@ -24,28 +24,22 @@ func main() {
 
 	args := os.Args
 	for _, arg := range args {
-		if arg == "-h" {
+		if arg == "-h" || arg == "--help" {
 			fmt.Printf("Usage: %s [options...] <url>\nWill spam 'curl' command until receive SIGINT\nBelow is curl help\n==============================================\n", args[0])
 			args[0] = "curl"
-			syscall.Exec(path, args, os.Environ())
-			break
+			cmd := exec.Command(path, args...)
+			stdout, err := cmd.Output()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(string(stdout))
+			os.Exit(0)
 		}
 	}
 	args[0] = "curl"
 	for {
-		pid, _, _ := syscall.Syscall(syscall.SYS_FORK, 0, 0, 0)
-		if pid == 0 {
-			syscall.Exec(path, args, os.Environ())
-		} else {
-			proc, err := os.FindProcess(int(pid))
-			if err != nil {
-				panic("Something is wrong in spawning processes")
-			}
-			_, err = proc.Wait()
-			if err != nil {
-				panic(err.Error())
-			}
-		}
+		cmd := exec.Command(path, args...)
+		stdout, _ := cmd.Output()
+		fmt.Println(string(stdout))
 	}
-
 }
